95202b9b5be61aedbacabf49dec6fa1a7e49f5c7
Andreas Nachtmann <virtualnonsense@mailbox.org>
2022-02-17 21:33:36 +0100

    quick and dirty cleanup

--- clock.py
+++ clock.py
@@ -25,12 +25,13 @@ class Clock:
             roll_indices = [i for i, (new_c, old_c) in enumerate(zip(t, self.current_time_string)) if new_c != old_c]
             new_letters = []
             old_letters = []
+            clean_up = self.font[" "]
             x = 1
             y = 0
             for i, (new_c, old_c) in enumerate(zip(t, self.current_time_string)):
                 new_c_m = self.font[new_c]
                 old_c_m = self.font[old_c]
-                new_letters += [[x, -self.matrix.height, new_c_m]]
+                new_letters += [[x, -self.matrix.height - self.matrix.height // 3, new_c_m]]
                 old_letters += [[x, y, old_c_m]]
                 x += new_c_m.shape[1] + 1
 
@@ -42,11 +43,13 @@ class Clock:
                     self.matrix.set_cursor(x, old_y)
                     self.matrix.matrix_write(old_m)
                     continue
-                for offset in range(self.matrix.height):
-                    self.matrix.set_cursor(x, new_y + offset)
-                    self.matrix.matrix_write(new_m)
+                for offset in range(self.matrix.height + self.matrix.height // 3):
                     self.matrix.set_cursor(x, old_y + offset)
                     self.matrix.matrix_write(old_m)
+                    self.matrix.set_cursor(x, old_y + offset - old_m.shape[0])
+                    self.matrix.matrix_write(clean_up)
+                    self.matrix.set_cursor(x, new_y + offset)
+                    self.matrix.matrix_write(new_m)
                     self.matrix.update_matrix()
                     time.sleep(self.delay)
                 self.matrix.set_cursor(x, old_y)
