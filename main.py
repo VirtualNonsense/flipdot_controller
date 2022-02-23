8b097f4a281e49e0d250fd1734ad666b9ce57ca6
Andreas Nachtmann <virtualnonsense@mailbox.org>
2022-02-23 22:40:36 +0100

    using digital clock

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flipdot import FlipDotMatrix
 
 if __name__ == '__main__':
     m = FlipDotMatrix("192.168.47.9", 69)
-    clock = AnalogClock(m)
+    clock = DigitalClock(m)
     while True:
         clock.update()
         time.sleep(1)
